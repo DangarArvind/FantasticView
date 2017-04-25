e2766a3d8055d766eed50f1e1b53a19c7cf24728
Ankit Dave <ankitdave@195-MAC-9.local>
2017-04-25 16:17:20 +0530

    init

---
+++ FantasticView/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FantasticView
+//
+//  Created by Ankit Dave on 25/04/17.
+//  Copyright © 2017 Ankit Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let fantasticView = FantasticView(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
